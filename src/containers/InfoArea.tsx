e09ab76c16a9c028d1ecfc724062e127df54d978
Alex Okonechnikov <avo354@g.harvard.edu>
2019-01-20 21:18:37 -0500

    card draw

--- src/containers/InfoArea.tsx
+++ src/containers/InfoArea.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { moveCards as moveCardsType } from '../actions/zonesActions';
+import { PlayerData } from '../selectors/player';
 
-export default class InfoArea extends React.PureComponent {
+interface InfoAreaProps {
+    player: PlayerData;
+    moveCards: moveCardsType;
+}
+
+export default class InfoArea extends React.PureComponent<InfoAreaProps> {
     public render() {
+        const { player, moveCards } = this.props;
+        const { hand, library } = player;
+        const drawCard = () => moveCards(library.id, [library.cards[0].id], hand.id, 0);
         return (
-            <h1>This is info area</h1>
+            <h1 onClick={drawCard}>This is info area</h1>
         );
     }
 }
