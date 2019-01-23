a47fc7a9b48987607315a14bad43cdc44c216da0
Alex Okonechnikov <avo354@g.harvard.edu>
2019-01-23 13:03:49 -0500

    deselct on drag end

--- src/reducers/selectReducer.ts
+++ src/reducers/selectReducer.ts
@@ -1,5 +1,6 @@
 import produce from 'immer';
 import { SelectCardsAction } from '../actions/selectActions';
+import { MoveCardsAction } from '../actions/zonesActions';
 
 // Single boolean loading value to be toggled
 
@@ -13,13 +14,19 @@ const defaultState: SelectState = {
     selected: []
 };
 
-export default function selectReducer(state: SelectState = defaultState, action: SelectCardsAction): SelectState {
+type Actions = SelectCardsAction & MoveCardsAction;
+
+export default function selectReducer(state: SelectState = defaultState, action: Actions): SelectState {
     return produce(state, (draft) => {
         switch (action.type) {
             case 'SELECT_CARDS':
                 const { cards } = action.payload;
                 const selected = draft.selected;
                 selected.splice(0, selected.length, ...cards);
+                break;
+            case 'MOVE_CARDS':
+                const clear = draft.selected;
+                clear.splice(0, clear.length);
                 break;
         }
     });
