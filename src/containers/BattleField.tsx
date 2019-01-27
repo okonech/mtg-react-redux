e548f91c46a97e4ebccc9a37c61b901a912576e2
Alex Okonechnikov <avo354@g.harvard.edu>
2019-01-27 01:54:13 -0500

    aaa

--- src/containers/BattleField.tsx
+++ src/containers/BattleField.tsx
@@ -1,6 +1,10 @@
 
 import React from 'react';
-import { ConnectDropTarget, DropTarget, DropTargetSpec } from 'react-dnd';
+import {
+    ConnectDragPreview, ConnectDragSource, ConnectDropTarget, DragSource,
+    DragSourceSpec, DropTarget, DropTargetSpec
+} from 'react-dnd';
+import { getEmptyImage } from 'react-dnd-html5-backend';
 import { findDOMNode } from 'react-dom';
 import { SelectableGroup, SelectableItem } from 'react-selectable-fast';
 import { selectCards as selectCardsType } from '../actions/selectActions';
@@ -8,6 +12,7 @@ import { moveCards as moveCardsType } from '../actions/zonesActions';
 import DraggableCard, { CardDragObject } from '../components/DraggableCard';
 import { Types } from '../Constants';
 import { CardCoordZone } from '../selectors/player';
+import { coordInNode, vhToPx } from '../util/coordinates';
 
 const BattleFieldStyle: React.CSSProperties = {
     height: '100%',
@@ -26,6 +31,7 @@ const battlefieldTarget: DropTargetSpec<BattleFieldProps> = {
         const { zoneId, cards, initialX, initialY } = monitor.getItem() as CardDragObject;
         const node = findDOMNode(component) as Element;
         const bounds = node.getBoundingClientRect();
+        console.log(initialX, initialY);
 
         const xCoord = monitor.getClientOffset().x - bounds.left - initialX;
         const yCoord = monitor.getClientOffset().y - bounds.top - initialY;
@@ -33,6 +39,52 @@ const battlefieldTarget: DropTargetSpec<BattleFieldProps> = {
         moveCards(zoneId, cards, zone.id, zone.cards.length, xCoord, yCoord);
     }
 };
+
+const battleFieldSource: DragSourceSpec<BattleFieldProps, CardDragObject> = {
+
+    beginDrag(props, monitor, component: BattleField) {
+
+        const { selected, zone, selectCards, cardHeight } = props;
+        console.log('Start drag ' + selected);
+
+        const { x, y } = coordInNode(component, monitor.getInitialClientOffset());
+        const cardHeightPx = vhToPx(cardHeight);
+        const cardWidthPx = cardHeightPx / 1.45;
+
+        // locate card at coord
+
+        const dragCard = zone.cards.find((card) => {
+            const { xCoord, yCoord } = card;
+            console.log(x, y, xCoord, yCoord, cardWidthPx, cardHeightPx);
+            return (xCoord < x) && (xCoord + cardWidthPx >= x) && (yCoord < y) && (yCoord + cardHeightPx >= y);
+        });
+        const { id, name } = dragCard;
+
+        const cards = [...selected];
+        if (!cards.includes(id)) {
+            cards.push(id);
+        }
+        selectCards(cards);
+        return {
+            cards,
+            firstName: name,
+            zoneId: zone.id,
+            initialX: x,
+            initialY: y
+        };
+    },
+
+    endDrag(props, monitor) {
+        console.log('End drag ' + props.selected);
+        // todo: move drop call logic into droptarget drop method
+        // also add droptarget drop on hand and battlefield, putting cards at end of list
+
+        // trello board handles preview drag by passing isover as a prop, and adding a moved 
+        // placeholder in the render method
+        // do this with cards for sort
+    }
+};
+
 interface BattleFieldProps {
     zone: CardCoordZone;
     moveCards: moveCardsType;
@@ -48,13 +100,21 @@ interface BattleFieldTargetCollectedProps {
     item: CardDragObject;
 }
 
+interface BattleFieldSourceCollectedProps {
+    connectDragSource: ConnectDragSource;
+    connectDragPreview: ConnectDragPreview;
+    isDragging: boolean;
+}
+
 interface BattleFieldState {
     selectEnabled: boolean;
 }
 
-class BattleField extends React.PureComponent<BattleFieldProps & BattleFieldTargetCollectedProps, BattleFieldState>  {
+type AllProps = BattleFieldProps & BattleFieldSourceCollectedProps & BattleFieldTargetCollectedProps;
 
-    constructor(props: BattleFieldProps & BattleFieldTargetCollectedProps) {
+class BattleField extends React.PureComponent<AllProps, BattleFieldState>  {
+
+    constructor(props: AllProps) {
         super(props);
 
         this.state = {
@@ -62,65 +122,87 @@ class BattleField extends React.PureComponent<BattleFieldProps & BattleFieldTarg
         };
     }
 
-    public mouseEnter = ((event: any) => (this.props.item ? null : this.setState({ selectEnabled: false })));
+    public componentDidMount() {
+        const { connectDragPreview } = this.props;
+        if (connectDragPreview) {
+            // Use empty image as a drag preview so browsers don't draw it
+            // and we can draw whatever we want on the custom drag layer instead.
+            connectDragPreview(getEmptyImage(), {
+                // IE fallback: specify that we'd rather screenshot the node
+                // when it already knows it's being dragged so we can hide it with CSS.
+                captureDraggingState: true
+            });
+        }
+    }
 
-    public mouseLeave = ((event: any) => (this.props.item ? null : this.setState({ selectEnabled: true })));
+    public selectDisabled = ((event: any) => (this.props.item ? null : this.setState({ selectEnabled: false })));
+
+    public selectEnabled = ((event: any) => (this.props.item ? null : this.setState({ selectEnabled: true })));
 
     public setSelected = (items: SelectableItem[]) => this.props.selectCards(items.map((item) => item.props.id));
 
     public clearSelected = () => this.props.selectCards([]);
 
     public render() {
-        const { zone, connectDropTarget, selected, cardHeight, selectCards, canDrop, item } = this.props;
+        const { zone, connectDropTarget, connectDragSource, selected, cardHeight, canDrop, item } = this.props;
         const { selectEnabled } = this.state;
-        // horrible hack to keep child mounted, so it can remain dragging but also look hidden
         const cards = zone.cards.reduce((acc, curr, idx) => {
-
+            if (canDrop && item.cards.includes(curr.id)) {
+                return acc;
+            }
             acc.push(
                 <DraggableCard
                     zoneId={zone.id}
                     name={curr.name}
                     id={curr.id}
                     key={'draggable' + curr.id}
-                    onMouseEnter={this.mouseEnter}
-                    onMouseLeave={this.mouseLeave}
+                    onMouseEnter={this.selectDisabled}
+                    onMouseLeave={this.selectEnabled}
                     selected={selected.includes(curr.id)}
-                    selectCards={selectCards}
                     cardHeight={cardHeight}
                     xCoord={curr.xCoord}
                     yCoord={curr.yCoord}
-                    hidden={canDrop && item.cards.includes(curr.id)}
                 />
             );
             return acc;
         },                              []);
 
         return (
-            connectDropTarget(
-                <div style={SelectableStyle} >
-                    <SelectableGroup
-                        ref={(ref) => ((window as any).selectableGroup = ref)}
-                        className='selectable'
-                        tolerance={0}
-                        deselectOnEsc={true}
-                        disabled={!selectEnabled}
-                        resetOnStart={true}
-                        onSelectionFinish={this.setSelected}
-                        onSelectionClear={this.clearSelected}
+            connectDragSource(
+                connectDropTarget(
+                    <div
+                        style={SelectableStyle}
+                        onMouseEnter={this.selectEnabled}
                     >
-                        <section style={BattleFieldStyle}>
-                            {cards}
-                        </section>
-                    </SelectableGroup>
-                </div >
+                        <SelectableGroup
+                            ref={(ref) => ((window as any).selectableGroup = ref)}
+                            className='selectable'
+                            tolerance={0}
+                            deselectOnEsc={true}
+                            disabled={!selectEnabled}
+                            resetOnStart={true}
+                            onSelectionFinish={this.setSelected}
+                            onSelectionClear={this.clearSelected}
+                        >
+                            <section style={BattleFieldStyle}>
+                                {cards}
+                            </section>
+                        </SelectableGroup>
+                    </div >
+                )
             )
         );
     }
 }
 
-export default DropTarget(Types.CARD, battlefieldTarget, (connect, monitor) => ({
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver(),
-    canDrop: monitor.canDrop(),
-    item: monitor.getItem()
-}))(BattleField);
+export default DragSource<BattleFieldProps, BattleFieldSourceCollectedProps>(
+    Types.CARD, battleFieldSource, (connect, monitor) => ({
+        connectDragSource: connect.dragSource(),
+        connectDragPreview: connect.dragPreview(),
+        isDragging: monitor.isDragging()
+    }))(DropTarget(Types.CARD, battlefieldTarget, (connect, monitor) => ({
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver(),
+        canDrop: monitor.canDrop(),
+        item: monitor.getItem()
+    }))(BattleField));
