ca72e03fbebb5d9d99ce492e5b209721733268ac
Alex Okonechnikov <avo354@g.harvard.edu>
2020-01-22 01:39:34 -0500

    a

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ set -o nounset  # exit on use of uninitialized variable
 set -o pipefail # exit when any part of a pipe fails
 
 lerna bootstrap --ci
-lerna run build --stream
+lerna run build --stream --scope={@mtg-react-redux/client,@mtg-react-redux/backup-restore-fb,@mtg-react-redux/tests}
